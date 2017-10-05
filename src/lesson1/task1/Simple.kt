866c5728fa2680fad94e3a09cb14b2afe5951fe8
ZotikovN <wwwkooly12@gmail.com>
2017-10-05 21:01:37 +0300

    lesson1/task1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -114,4 +114,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = initial+percent/10
  *Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
 fun numberRevert(number: Int): Int =
-        ((number/100)+((number/10-(number/100*10))*10)+((number-(number/10*10))*100))
+        ((number/100)+((number/10-(number/100*10))*10)+((number-(number/10*10))*100))11
